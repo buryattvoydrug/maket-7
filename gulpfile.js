82f0d9159a6607122f9303ebf8d19c7a0d1b3ffd
buryattvoydrug <49764701+buryattvoydrug@users.noreply.github.com>
2020-01-26 18:44:02 +0300

    BASE

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+//npm install gulp --save-dev
+//npm install browser-sync --save-dev
+//npm install gulp-sass --save-dev
+//npm install gulp-rename --save-dev
+//npm i gulp-sourcemaps
+//npm i gulp-autoprefixergulp
+var gulp=require('gulp');
+var sass=require('gulp-sass');
+var rename=require('gulp-rename');
+var sourcemaps=require('gulp-sourcemaps');
+var autoprefixer=require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
+function sass_(){
+  return gulp.src('app/scss/style.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      errorLogToConsole:true,
+      outputStyle:'compressed'
+  }))
+    .on('error',console.error.bind(console))
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+        }))
+    .pipe(rename({suffix:'.min'}))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.stream());
+}
+function browserReload (){
+    browserSync.reload();
+}
+function watch_(){
+    gulp.watch('app/scss/style.scss', sass_);
+    gulp.watch('app/*.html', browserReload);
+    gulp.watch('app/**/*.php', browserReload);
+    gulp.watch('app/**/*.js', browserReload);
+}
+function browserSync_(){
+    browserSync.init({
+       server:{
+           baseDir: 'app/'
+       },
+        port: 3000
+    });
+}
+gulp.task('default', gulp.parallel(browserSync_, watch_));
